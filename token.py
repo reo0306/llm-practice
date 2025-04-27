1f29837d70bc1e03e9cb1910edabe4e161a002c1
reo0306 <reo19840306@gmail.com>
2025-04-27 21:27:55 +0900

    first commit

---
+++ token.py
@@ -0,0 +1,14 @@
+import re
+
+text = "Hello, world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+result = [item.strip() for item in result if item.strip()]
+print(result)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
