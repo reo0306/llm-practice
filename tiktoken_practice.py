02621f344a86db9b7181ef917ea21485f0586937
reo0306 <reo19840306@gmail.com>
2025-04-30 06:48:02 +0900

    rename file, add file

---
+++ tiktoken_practice.py
@@ -0,0 +1,14 @@
+from importlib.metadata import version
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
