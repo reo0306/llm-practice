1b2d5216fd677667cdaa5d0b595c230e08742993
reo0306 <reo19840306@gmail.com>
2025-05-07 17:01:35 +0900

    add docker file

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM arm64v8/python:3.13
+
+RUN apt-get update
+RUN apt-get install -y sudo locales apt-utils tzdata init systemd
+RUN apt-get install -y locales && localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+RUN apt-get install -y vim less
+
+ENV TIMEZONE=Asia/Tokyo
+RUN ln -snf /usr/share/zoneinfo/$TIMEZONE /etc/localtime \
+    && echo $TIMEZONE > /etc/timezone 
+
+ENV LANG ja_JP.UTF-8
+ENV LANGUAGE ja_JP:ja
+ENV LC_ALL_ja_JP.UTF-8
+ENV TZ JST-9
+ENV TERM xterm
+
+RUN pip3 install --upgrade setuptools
+
+COPY requirements.txt .
+RUN pip3 install -r requirements.txt
+
+RUN /usr/bin/systemctl disable getty@tty1.service \
+  && /usr/bin/systemctl disable getty.target \
+  && /usr/bin/systemctl disable systemd-udevd.service
+
--- gpt_data_set_v1.py
+++ gpt_data_set_v1.py
@@ -1,4 +1,5 @@
 import torch
+import tiktoken
 from torch.utils.data import Dataset, DataLoader
 
 class GPTDatasetV1(Dataset):
@@ -18,3 +19,31 @@ class GPTDatasetV1(Dataset):
 
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text,
+    batch_size=1,
+    max_length=4,
+    stride=1,
+    shuffle=False
+)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
