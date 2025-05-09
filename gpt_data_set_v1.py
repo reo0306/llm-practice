e8eba4e9600074825acbbd2a4cf5cd02ea2d8be0
reo0306 <reo19840306@gmail.com>
2025-05-10 00:08:55 +0900

    update2

--- gpt_data_set_v1.py
+++ gpt_data_set_v1.py
@@ -10,7 +10,7 @@ class GPTDatasetV1(Dataset):
 
         for i in range(0, len(token_ids) - max_length, stride):
             input_chunk = token_ids[i:i + max_length]
-            target_chunk = token_ids[i + 1: i * max_length + 1]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
     
@@ -37,19 +37,29 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
+max_length = 4
 dataloader = create_dataloader_v1(
     raw_text,
-    batch_size=1,
-    max_length=4,
-    stride=4,
+    batch_size=8,
+    max_length=max_length,
+    stride=max_length,
     shuffle=False
 )
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
-#inputs, targets = next(data_iter)
-#print("Inputs:\n", inputs)
-#print("\nTargets:\n", targets)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
 
-#second_batch = next(data_iter)
-#print(second_batch)
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
